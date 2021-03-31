15df70b3f04983060c8e4e2fb819ced2f23c66a9
yprateek136 <yprateek195@gmail.com>
2021-03-31 18:31:01 +0530

    array_deletion

---
+++ array_deletion.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int deletion(int arr[],int size,int index)
+{
+	for(int i=index;i<size-1;i++)
+	{	
+		arr[i]=arr[i+1];
+	}
+	return 1;
+}
+void display(int arr[],int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
+int main()
+{
+	int arr[100]={11,13,17,19,27};
+	int size=5,index=4;
+	display(arr,size);
+	deletion(arr,size,index);
+	size-=1;
+	display(arr,size);
+	return 0;
+}

84933bddc1ef53e4b5fe233f12092b908ced1efe
yprateek136 <yprateek195@gmail.com>
2021-09-12 06:28:59 +0530

    Searching

---
+++ Find the element that appears once.cpp
@@ -0,0 +1,40 @@
+// Find the element that appears once.cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int search(int A[], int N)
+	{
+       int XOR = 0;
+        for (int i = 0; i < N; i++) 
+        {
+            XOR = XOR ^ A[i];
+        }
+        return XOR;
+	    
+	}
+};
+
+// { Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t,len;
+    cin>>t;
+    while(t--)
+    {
+        cin>>len;
+        int arr[len];
+        for(int i=0;i<len;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.search(arr, len)<<'\n';
+    }
+    return 0;
+}
+  // } Driver Code Ends

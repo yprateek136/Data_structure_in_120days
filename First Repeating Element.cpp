a97f7bf0655c17939900be5ad96bd77e010ce121
yprateek136 <yprateek195@gmail.com>
2021-09-24 22:31:38 +0530

    hash

---
+++ First Repeating Element.cpp
@@ -0,0 +1,48 @@
+/* ---First Repeating Element.cpp---*/
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function template in C++
+
+class Solution {
+  public:
+    // Function to return the position of the first repeating element.
+    int firstRepeated(int arr[], int n) {
+        // code here
+        unordered_map<int , int>ump;
+        for(int i=0;i<n;i++)
+        {
+            ump[arr[i]]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(ump[arr[i]]>1)
+            {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+
+        for (int i = 0; i < n; ++i) cin >> arr[i];
+        Solution ob;
+        cout << ob.firstRepeated(arr, n) << "\n";
+    }
+
+    return 0;
+}
+  // } Driver Code Ends

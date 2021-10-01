31dc7a42c80d53bef81fd9fc8b2d866dd1edc0bc
yprateek136 <yprateek195@gmail.com>
2021-10-01 11:54:58 +0530

    Stack

---
+++ Get the min at pop.cpp
@@ -0,0 +1,69 @@
+/*------------Get the min at pop.cpp------------*/
+#include <bits/stdc++.h>
+using namespace std;
+
+stack<int> _push(int arr[],int n);
+
+void _getMinAtPop(stack<int>s);
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+//Function to push all the elements into the stack.
+stack<int> _push(int arr[],int n)
+{
+   // your code here
+   stack<int>st;
+   int min=0;
+   for(int i=0;i<n;i++)
+   {
+       if(st.empty()){
+           min=arr[i];
+           st.push(arr[i]);
+       }
+       else{
+           if(arr[i] <st.top()){
+               st.push(arr[i]);
+               min=arr[i];
+           }
+           else{
+               st.push(min);
+           }
+       }
+   }
+   return st;
+}
+
+//Function to print minimum value in stack each time while popping.
+void _getMinAtPop(stack<int>s)
+{
+    // your code here
+    while(!s.empty())
+    {
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+}
+
+// { Driver Code Starts.
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    stack<int>mys=_push(arr,n);
+	    _getMinAtPop(mys);
+	    
+	    cout<<endl;
+	    
+	}
+	return 0;
+}
+
+  // } Driver Code Ends

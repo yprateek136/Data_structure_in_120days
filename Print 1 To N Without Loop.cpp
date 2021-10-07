413dd7d3c0c7b5c5bc450bdc84c21518b7320832
yprateek136 <yprateek195@gmail.com>
2021-10-07 18:52:17 +0530

    Tree

---
+++ Print 1 To N Without Loop.cpp
@@ -0,0 +1,40 @@
+/*---------Print 1 To N Without Loop.cpp-------*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    //Complete this function
+    void printNos(int N)
+    {
+        //Your code here
+        if(N>0)
+        {
+            printNos(N-1);
+            cout<<N<<" ";
+        }
+    }
+};
+
+// { Driver Code Starts.
+/* Driver program to test printNos */
+int main()
+{
+    int T;
+    
+    //taking testcases
+    cin>>T;
+    
+    while(T--)
+    {
+        int N;
+        
+        //input N
+        cin>>N;
+        Solution ob;
+        //calling printNos() function
+        ob.printNos(N);
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
